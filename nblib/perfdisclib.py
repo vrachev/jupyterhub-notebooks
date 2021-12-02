b8e4e101c2b36f2859ca6712dbf092d843135cc3
vrachev <vlad.rachev@mongodb.com>
2021-12-02 16:22:38 +0000

    more updates

--- nblib/perfdisclib.py
+++ nblib/perfdisclib.py
@@ -1,12 +1,16 @@
 import requests
-import typing as typ
 import pandas as pd
+import numpy as np
 import json
 from dataclasses import dataclass
+from more_itertools import pairwise
+
+from nblib import data
 
 
 @dataclass
 class Info:
+    client: data.ClientConnection
     max_tasks: int
     max_tests: int
     batch: int
@@ -112,3 +116,100 @@ class Analysis:
 
         return comparison, found_ts
 
+    @staticmethod
+    def get_stable_region(commit_date, ts, cps):
+        """Algorithm to look up time series in order to characterize the stable region of results around build_a."""
+
+        true_positive_orders = {
+            cp["order"]
+            for cp in cps
+            if cp["triage"]["triage_status"] == "true_positive"
+        }
+        len_ts = len(ts["data"])
+        stable_region_bounds = (
+                [0]
+                + [idx for idx, datum in enumerate(ts["data"]) if datum["order"] in true_positive_orders]
+                + [len_ts]
+        )
+
+        start = end = 0
+
+        # if base commit before or after the entire time series, get the closest stable region
+        if commit_date < ts["data"][0]["commit_date"]:
+            # first stable region
+            start = stable_region_bounds[0]
+            end = stable_region_bounds[1]
+
+        if commit_date > ts["data"][len_ts - 1]["commit_date"]:
+            # last stable region
+            start = stable_region_bounds[-2]
+            end = stable_region_bounds[-1]
+
+        for start_bound, end_bound in pairwise(stable_region_bounds):
+            if (
+                    ts["data"][start_bound]["commit_date"]
+                    <= commit_date
+                    <= ts["data"][end_bound - 1]["commit_date"]
+            ):
+                start = start_bound
+                end = end_bound
+        return [datum["value"] for datum in ts["data"][start:end]]
+
+    def process_stable_regions(self, found_ts):
+        """Calculate the means and std dev for the Zscores"""
+        # limit number of tests
+        found_ts = found_ts[0:self.pd_info.max_tests]
+
+        total = len(found_ts)
+
+        stable_mean = []
+        stable_std = []
+        stable_length = []
+
+        date_a = self.pd_info.client["expanded_metrics"]["versions"].find_one({"version_id": self.pd_info.build_a})["commit_date"]
+        date_b = self.pd_info.client["expanded_metrics"]["versions"].find_one({"version_id": self.pd_info.build_b})["commit_date"]
+
+        for index, row in found_ts.iterrows():
+            # some tests do not have threads.
+            if row["args"] == "null":
+                row["args"] = "{}"
+            ts = self.pd_info.client["expanded_metrics"]["time_series"].find_one({
+                "project": row["project"],
+                "variant": row["variant"],
+                "task": row["task"],
+                "test": row["test"],
+                "args": json.loads(row["args"]),
+                "measurement": row["measurement"],
+            })
+            cps = list(self.pd_info.client["expanded_metrics"]["change_points"].find({
+                "time_series_info.project": row["project"],
+                "time_series_info.variant": row["variant"],
+                "time_series_info.task": row["task"],
+                "time_series_info.test": row["test"],
+                "time_series_info.args": json.loads(row["args"]),
+                "time_series_info.measurement": row["measurement"],
+            }))
+
+            try:
+                stable_region = self.get_stable_region(date_a, ts, cps)
+                stable_mean.append(np.mean(stable_region))
+                stable_std.append(np.std(stable_region))
+                stable_length.append(len(stable_region))
+            except Exception as e:
+                # no stable region found
+                print("")
+                print(f"no stable region found for {len(stable_length)}")
+                print("")
+                stable_mean.append(np.nan)
+                stable_std.append(np.nan)
+                stable_length.append(0)
+                pass
+
+            print(f"Stable regions processed: {len(stable_length)}/{total}", end="\r")
+
+        print('')
+        found_ts.insert(0, "stable_mean", stable_mean)
+        found_ts.insert(1, "stable_std", stable_std)
+        found_ts.insert(2, "stable_length", stable_length)
+
+        return found_ts, date_a, date_b
